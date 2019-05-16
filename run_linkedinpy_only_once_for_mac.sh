516f778e927ef2109896037c985f27fd9e619a7e
ishan dutta <ishandutta2007@gmail.com>
2019-05-16 14:38:45 +0530

    Update run_linkedinpy_only_once_for_mac.sh

--- run_linkedinpy_only_once_for_mac.sh
+++ run_linkedinpy_only_once_for_mac.sh
@@ -2,7 +2,7 @@
 
 # This is a script to mitigate possibility of multiple parallel cron jobs being triggered(discussed here: https://github.com/timgrossmann/InstaPy/issues/1235)
 # The following is an example of a cron scheduled every 10 mins
-# */10 * * * * bash /path/to/LinkedinPy/run_linkedinpy_only_once_for_mac.sh /path/to/LinkedinPy/quickstart.py
+# */10 * * * * bash /path/to/LinkedinPy/run_linkedinpy_only_once_for_mac.sh /path/to/LinkedinPy/quickstart.py $USERNAME $PASSWORD
 
 TEMPLATE_PATH=$1
 USERNAME=$2
@@ -21,4 +21,3 @@ else
    echo "Starting $TEMPLATE_PATH"
    /Users/ishandutta2007/.pyenv/shims/python $TEMPLATE_PATH -u $USERNAME -p $PASSWORD --headless-browser --disable_image_load
 fi
-
