a183b071f5861fd96ca3b540d52bbfd8f187b7da
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-05-16 05:54:55 +0530

    minor

--- run_linkedinpy_only_once_for_mac.sh
+++ run_linkedinpy_only_once_for_mac.sh
@@ -2,7 +2,7 @@
 
 # This is a script to mitigate possibility of multiple parallel cron jobs being triggered(discussed here: https://github.com/timgrossmann/InstaPy/issues/1235)
 # The following is an example of a cron scheduled every 10 mins
-# */10 * * * * bash /path/to/instapy-scripts/run_instapy_only_once_for_mac.sh /path/to/instapy/quickstart_templates/target_followers_of_similar_accounts_and_influencers.py
+# */10 * * * * bash /path/to/LinkedinPy/run_linkedinpy_only_once_for_mac.sh /path/to/LinkedinPy/quickstart.py
 
 TEMPLATE_PATH=$1
 USERNAME=$2
